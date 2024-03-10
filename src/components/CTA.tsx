98541c5dcabb6cf77e604d0349606e99ccdfdae8
denimozh <denimozh@gmail.com>
2024-03-10 22:30:24 +0000

    Finished CTA section and Footer

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import nft2 from "./images/nft2.png"
+import Image from 'next/image'
 
 const CTA = () => {
   return (
     <div className='bg-gradient-to-r from-blue-400 to-blue-600 border-2 border-black animate-bgMove rounded-t-[40px] h-[500px]'>
-        <div className='flex flex-row p-14'>
-          <div className='basis-[60%]'>
+        <div className='flex flex-row p-14 gap-32'>
+          <div className='flex flex-col justify-between basis-[60%] pb-5'>
             <p className='text-6xl text-white'>Invest into the Future Of Art</p>
             <p className='text-2xl'>Come and Explore our Marketplace to see and purchase amazing digital NFT Art</p>
-            <div className='bg-blue-200 border-2 flex border-black w-[250px] h-[90px] justify-center items-center rounded-2xl hover:border-r-4 hover:border-b-4'>
+            <div className='cursor-pointer bg-blue-200 border-2 flex border-black w-[250px] h-[90px] justify-center items-center rounded-2xl hover:border-r-4 hover:border-b-4'>
                 <p className='text-3xl'>Start Now</p>
             </div>
           </div>
-          <div>
-
+          <div className='basis-[40%]'>
+            <div className='rounded-[20px]'>
+              <Image src={nft2} width={400} height={550} alt="a" className='rounded-[20px] border-l-8 border-b-8 border-black'/>
+            </div>
           </div>
         </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { IoLogoInstagram } from "react-icons/io";
+import { IoLogoFacebook } from "react-icons/io";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='px-40 flex flex-row pt-20 '>
+      <div className='bg-[#5468CD] w-full flex flex-col gap-14 rounded-t-[20px] p-10 border-t-2 border-x-2 border-black'>
+        <div className='flex flex-row justify-between'>
+          <div>
+            <p className='text-3xl text-white'>nft<b className='text-blue-800'>dev</b></p>
+          </div>
+          <div className='flex flex-row gap-10'>
+            <p className='text-[40px]'><IoLogoInstagram/></p>
+            <p className='text-[40px]'><IoLogoFacebook/></p>
+            <p className='text-[40px]'><IoLogoTwitter/></p>
+          </div>
+        </div>
+        <div className='flex flex-row justify-between'>
+            <div>
+              <p>2024 nftdev All Rights Reserved</p>
+            </div>
+            <div className='flex flex-row gap-8'>
+              <p className='underline'>Privacy Policy</p>
+              <p className='underline'>Terms Of Service</p>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
