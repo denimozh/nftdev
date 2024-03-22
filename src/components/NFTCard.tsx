9bf9f0f0388f2de23a31e9c8ad60ef30c913891b
denimozh <denimozh@gmail.com>
2024-03-22 22:52:57 +0000

    Started implemeting Props to NFTCard

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,21 +1,22 @@
+
 import React from 'react'
 
-const NFTCard = () => {
+const NFTCard = ({name}:{name:string}, {price}:{price:number}, {img}:{img:string}) => {
   return (
     <div className='flex flex-col p-4'>
-      <div>
-
+      <div className='w-full h-1/2'>
+        <img src={img} alt='123'/>
       </div>
       <div className='flex flex-row justify-between'>
-        <p></p>
+        <p>{name}</p>
         <p></p>
       </div>
       <div>
         <p></p>
       </div>
       <div className='flex flex-row justify-between'>
-        <p></p>
-        <p></p>
+        <p>Price</p>
+        <p>{price}</p>
       </div>
     </div>
   )
