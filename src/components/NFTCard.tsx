49ffdaf841a6cf345783e695e50233a50566422a
denimozh <denimozh@gmail.com>
2024-03-28 23:43:27 +0000

    Added Heart Function to NFTCard

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,6 +1,7 @@
-
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { FaEthereum } from "react-icons/fa";
+import { FaHeart } from 'react-icons/fa6';
 
 interface Props{
   name: string,
@@ -9,8 +10,16 @@ interface Props{
 }
 
 const NFTCard: React.FC<Props> = ({name, price, img}) => {
+  const [heart, setHeart] = useState(false);
+  let color = "";
+  if (heart == true){
+    color = "text-red-500"
+  } else {
+    color = "text-white"
+  }
+
   return (
-    <div className='flex flex-col p-4'>
+    <div className='relative flex flex-col p-4'>
       <div className='bg-white p-5 rounded-[30px]'>
         <div className=''>
           <img src={img} alt='123' className='h-[450px] w-[400px] rounded-[20px]'/>
@@ -24,6 +33,12 @@ const NFTCard: React.FC<Props> = ({name, price, img}) => {
             <p className='text-xl text-gray-400'>Price</p>
             <p className='pr-1 text-2xl font-semibold text-blue-600'>{price} ETH</p>
           </div>
+        </div>
+        <div className='group absolute top-12 right-12 rounded-full backdrop-blur-xl border bg-gray-400 border-white ease-in-out duration-500'>
+            <p onClick={() => setHeart(!heart)} 
+            className={`text-[24px] p-[10px] ${color} group-hover:text-red-400 group-active:text-red-500 ease-in-out duration-500`}>
+                <FaHeart/>
+            </p>
         </div>
       </div>
     </div>
