a102e96a246e414b373fade1af20b05a5a431384
denimozh <denimozh@gmail.com>
2024-03-31 23:51:32 +0100

    Added Data for the NFT Products

---
+++ src/components/ListOfNfts/NFTList.ts
@@ -0,0 +1,10 @@
+export const nfts = [
+    {
+        id: 1,
+        title: "Island Hopper",
+        priceETH: 1.4,
+        priceBTC: 0.0032,
+        USDT: 2432,
+        img: '/nft1.png'
+    },
+]
